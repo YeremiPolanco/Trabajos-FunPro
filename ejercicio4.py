8517ac331cb441e1261503e2e876a79df01d2979
Juan Rime Garay <31824019+rimegaray@users.noreply.github.com>
2023-01-24 15:45:32 -0500

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
-print("PROGRAMA QUE AYUDA A SABER EL SUELDO DE UN EMPLEADO MAS SUS COMISIONES")
-suledo_base = float(input("ingrese su sueldo base: "))
+print("PROGRAMA QUE AYUDA A SABER EL SUELDO DE UN EMPLEADO MÁS SUS COMISIONES")
+sueldo_base = float(input("ingrese su sueldo base: "))
 venta_1 = float(input("ingrese el monto de su primera venta "))
 venta_2 = float(input("ingrese el monto de su segunda venta "))
 venta_3 = float(input("ingrese el monto de su tercera venta "))
@@ -8,11 +8,11 @@ v1_porcentaje = venta_1 * 0.10
 v2_porcentaje = venta_2 * 0.10
 v3_porcentaje = venta_3 * 0.10
 
-sueldo_total = suledo_base + v1_porcentaje + v2_porcentaje + v3_porcentaje
+sueldo_total = sueldo_base + v1_porcentaje + v2_porcentaje + v3_porcentaje
 comision = v1_porcentaje + v2_porcentaje + v3_porcentaje
 
 print("-------------------------------------------------------------------")
 print("BOLETA DE SUELDO LISTA :)")
-print("su sueldo base es: ", suledo_base)
-print("el procentaje total ganado es de: ", comision)
-print("conciderando lo anterior su sueldo es de: ", sueldo_total)
+print("su sueldo base es: ", sueldo_base)
+print("el porcentaje total ganado es de: ", comision)
+print("considerando lo anterior su sueldo es de: ", sueldo_total)
