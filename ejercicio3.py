dc9d8e0063a9a48165fa7f9cd8d55b4062cf288c
Juan Rime Garay <31824019+rimegaray@users.noreply.github.com>
2023-01-24 15:42:18 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,6 +5,6 @@ urgencias = presupuesto * 0.37
 pediatria = presupuesto * 0.42
 traumatologia = presupuesto * 0.21
 
-print("el presupiuesto de urgencias es: ", urgencias, "que representaria al 37%")
-print("el presupiuesto de pediatria es: ", pediatria, "que representaria al 42%")
-print("el presupiuesto de traumatologia es: ", traumatologia, "que representaria al 21%")
+print("el presupuesto de urgencias es: ", urgencias, "que representaría al 37%")
+print("el presupuesto de pediatría es: ", pediatria, "que representaría al 42%")
+print("el presupuesto de traumatología es: ", traumatologia, "que representaría al 21%")
